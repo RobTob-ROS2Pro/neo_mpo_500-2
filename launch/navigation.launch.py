32c7208e39cfdc6ce47e68496edb47698922ef90
RobTob-ROS2Pro <129752888+RobTob-ROS2Pro@users.noreply.github.com>
2023-06-06 11:06:22 +0200

    Update navigation.launch.py

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
         default=os.path.join(
             get_package_share_directory('neo_mpo_500-2'),
             'configs/navigation/maps',
-            'map_name.yaml'))
+            'dritte_map.yaml'))
 
     param_file_name = 'navigation.yaml'
     param_dir = LaunchConfiguration(
